c35058fc3ce2f3f348dd2e73b38e77034bcc57b1
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-07 03:46:07 +0530

    Delete operations

--- mongodb.js
+++ mongodb.js
@@ -33,15 +33,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error,client) => {
 //         console.log(error)
 //     })
 
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
-    }).then((result) => {
-        console.log(result.modifiedCount)
-    }).catch((error) => {
-        console.log(error)
-    })
+//    db.collection('users').deleteMany({
+//        age: 24
+//    }).then((result) => {
+//        console.log(result)
+//    }).catch((error) => {
+//        console.log(error)
+//    })
+
+        db.collection('tasks').deleteOne({
+           description: 'Clean the house'
+       }).then((result) => {
+           console.log(result)
+       }).catch((error) => {
+           console.log(error)
+       })
 })
