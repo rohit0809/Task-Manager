ef42bc865bc7281972509b85c36be5424ef0c0b9
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-08 03:15:35 +0530

    Validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 const databaseName = 'task-manager-api'
@@ -8,18 +9,49 @@ mongoose.connect(connectionURL, {
     useCreateIndex: true
 })
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                console.log('Email is invalid!!!')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0){
+                throw new Error('Age must be a positive number!!!')
+            }
+        }
+    }
+})
 
 // const me = new User({
-//     name: 'Rohit',
-//     age: 25
+//     name: 'Rohit Anand    ',
+//     email: 'ROHITANAND@GMAIL.COM',
+//     password: 'lovenodejs'
 // })
 
 // me.save().then(() => {
@@ -30,10 +62,13 @@ mongoose.connect(connectionURL, {
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
