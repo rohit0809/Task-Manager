a1062eb0d090518866c3b6e6892ee0006ef9a79f
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-15 00:33:51 +0530

    Authenticating Read Profile

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
-app.use((req,res,next) => {
-    res.status(503).send('Site is under Maintainance.Try after some time!!')
-})
+// app.use((req,res,next) => {
+//     res.status(503).send('Site is under Maintainance.Try after some time!!')
+// })
 
 app.use(express.json())
 app.use(userRouter)
