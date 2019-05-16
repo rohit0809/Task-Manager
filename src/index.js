39c1593bc8bd25a16a6746e8d91b06ddf3ddc5ca
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-17 01:24:51 +0530

    Formating Images using Sharp

--- src/index.js
+++ src/index.js
@@ -6,27 +6,6 @@ const multer = require('multer')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a word document!!!'))
-        }
-        cb(undefined, true)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req,res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
