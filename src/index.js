d9e87ebb7b496d34bc2bbb28c80a7b4fdb222538
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-17 00:15:35 +0530

    File Validation

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(doc|docx)$/)){
+            return cb(new Error('Please upload a word document!!!'))
+        }
+        cb(undefined, true)
+    }
 })
 
 app.post('/upload', upload.single('upload'), (req,res) => {
