6b3eb865761523143a9ed7af65bfb4c6e61aa12c
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-25 03:30:37 +0530

    Welcome and Deletion mails

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
-const { sendWelcomeEmail } = require('../emails/account')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -77,6 +77,7 @@ router.patch('/users/me', auth, async (req,res) => {
 router.delete('/users/me', auth, async (req,res) => {
     try{
         await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     }catch(e) {
         res.status(500).send()
