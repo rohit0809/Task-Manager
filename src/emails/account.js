a39c47d5111aa4d871541da3ff71d77059a524dc
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-25 04:17:03 +0530

    Environment Variables

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,7 +1,6 @@
 const sgMail = require('@sendgrid/mail')
-const sendgridAPIKey = ''
 
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email,name) => {
     sgMail.send({
